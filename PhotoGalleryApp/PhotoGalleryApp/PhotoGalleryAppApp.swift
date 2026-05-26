c0a3dba64f083790a84f3cba7042e7be53bc6fe5
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-05-26 09:07:18 +0530

    config app

---
+++ PhotoGalleryApp/PhotoGalleryApp/PhotoGalleryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoGalleryAppApp.swift
+//  PhotoGalleryApp
+//
+//  Created by Arjun Saseendran on 26/05/26.
+//
+
+import SwiftUI
+
+@main
+struct PhotoGalleryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhotoGalleryApp/PhotoGalleryApp/Views/PhotoGalleryView.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoGalleryView.swift
+//  PhotoGalleryApp
+//
+//  Created by Arjun Saseendran on 26/05/26.
+//
+
+import SwiftUI
+
+struct PhotoGalleryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PhotoGalleryView()
+}
