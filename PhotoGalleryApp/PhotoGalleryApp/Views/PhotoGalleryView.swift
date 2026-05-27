54809368e73dc5f0453cc7a4f12d4dd5c88e9cf9
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-05-27 08:54:58 +0530

    add enlarged photo view

--- PhotoGalleryApp/PhotoGalleryApp/Views/PhotoGalleryView.swift
+++ PhotoGalleryApp/PhotoGalleryApp/Views/PhotoGalleryView.swift
@@ -14,7 +14,9 @@ struct PhotoGalleryView: View {
         NavigationStack {
             VStack {
                 if let selectedImage {
-                   // todo
+                    EnlargedPhotoView(imageName: selectedImage){
+                        self.selectedImage = nil
+                    }
                 } else {
                     ScrollView {
                         VStack(spacing: 20) {
@@ -38,6 +40,37 @@ struct PhotoGalleryView: View {
 
             }.navigationTitle("Photo Gallery")
         }
+    }
+}
+
+struct EnlargedPhotoView: View {
+    let imageName: String
+    let onClose: () -> Void
+    var body: some View {
+        ZStack{
+            Color.black.ignoresSafeArea()
+            VStack{
+                Image(imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+                    .background(.white)
+                    .clipShape(.rect(cornerRadius: 20))
+                    .shadow(radius: 10)
+                
+                Button(action: onClose){
+                    Text("Close")
+                        .foregroundStyle(.white)
+                        .padding()
+                        .background(.red)
+                        .clipShape(.rect(cornerRadius: 10))
+                    
+                }
+                .padding(.top, 20)
+            }
+        }
+        
+        
     }
 }
 
