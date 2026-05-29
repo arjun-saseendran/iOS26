4d382d81bbc8d8bac5cf0c6281c4be435f6d538e
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-05-29 16:11:50 +0530

    add snow fall

---
+++ SnowfallAnimation/SnowfallAnimation/SnowfallAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowfallAnimationApp.swift
+//  SnowfallAnimation
+//
+//  Created by Arjun Saseendran on 29/05/26.
+//
+
+import SwiftUI
+
+@main
+struct SnowfallAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
