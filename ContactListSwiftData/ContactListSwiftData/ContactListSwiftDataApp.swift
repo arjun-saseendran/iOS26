2c570e6b7dc8acda170d2b1d22ff2f859d6d777e
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-06-01 10:50:59 +0530

    add  friend model

---
+++ ContactListSwiftData/ContactListSwiftData/ContactListSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSwiftDataApp.swift
+//  ContactListSwiftData
+//
+//  Created by Arjun Saseendran on 01/06/26.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
