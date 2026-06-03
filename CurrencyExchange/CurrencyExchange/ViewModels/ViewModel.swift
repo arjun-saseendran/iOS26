7cf583da4d5ca37fb294ffdab9503b95ba32b390
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-06-03 11:52:57 +0530

    add view model

---
+++ CurrencyExchange/CurrencyExchange/ViewModels/ViewModel.swift
@@ -0,0 +1,65 @@
+//
+//  ViewModel.swift
+//  CurrencyExchange
+//
+//  Created by Arjun Saseendran on 03/06/26.
+//
+
+import Combine
+import Foundation
+import Observation
+
+@Observable
+final class ViewModel {
+    var exchagneRate: ExchangeRate?
+    private var cancellableSet: Set<AnyCancellable> = []
+
+    init() {
+        fetchRates()
+    }
+
+    func fetchRates() {}
+
+    deinit {
+        cancellableSet.forEach { $0.cancel() }
+    }
+}
+
+extension ViewModel {
+    func validateOutput() -> [Dictionary<String, Double>.Keys.Element] {
+        guard let output = exchagneRate?.rates?.keys.sorted() else { return [] }
+        return output
+    }
+
+    func emojiFlag(_ currencyCode: String) -> String {
+        guard let country = Country.getCountryBy(text: currencyCode) else {
+            return
+                currencyCode
+                .dropLast()
+                .unicodeScalars
+                .map({ 127397 + $0.value })
+                .compactMap(UnicodeScalar.init)
+                .map(String.init)
+                .joined()
+        }
+        return country.flagEmoji
+    }
+
+    func countryName(currencyCode: String) -> String? {
+        Country.getCountryBy(text: currencyCode)?.countryName
+    }
+
+    func formatRateForLocale(for key: String) -> String {
+        guard let mainRates = exchagneRate?.rates else { return "" }
+        let rate = mainRates.filter { $0.key == key }
+        var formatter: NumberFormatter {
+            let fm = NumberFormatter()
+            fm.numberStyle = .currency
+            fm.locale = Locale(
+                identifier: key.dropLast() + "_" + key.dropLast().uppercased()
+            )
+            return fm
+        }
+        return formatter.string(from: NSNumber(value: rate[key] ?? 1.0))!
+    }
+}
