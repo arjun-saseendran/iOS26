da0ca5a122229c12d159e90a7115b35d59feeb0d
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-06-03 08:37:23 +0530

    config app and add country model

---
+++ CurrencyExchange/CurrencyExchange/CurrencyExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyExchangeApp.swift
+//  CurrencyExchange
+//
+//  Created by Arjun Saseendran on 03/06/26.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
