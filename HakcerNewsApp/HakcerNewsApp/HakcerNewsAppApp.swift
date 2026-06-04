abebe1fb6eb73f669d18ea6a67408afef41bcbd8
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-06-04 07:19:24 +0530

    config app and add post model

---
+++ HakcerNewsApp/HakcerNewsApp/HakcerNewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HakcerNewsAppApp.swift
+//  HakcerNewsApp
+//
+//  Created by Arjun Saseendran on 04/06/26.
+//
+
+import SwiftUI
+
+@main
+struct HakcerNewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HakcerNewsApp/HakcerNewsApp/Model/Post.swift
@@ -0,0 +1,21 @@
+//
+//  Post.swift
+//  HakcerNewsApp
+//
+//  Created by Arjun Saseendran on 04/06/26.
+//
+
+import Foundation
+
+struct Results: Decodable {
+    let hits: [Post]
+}
+
+
+struct Post: Decodable, Identifiable {
+    var id: String { return objectID}
+    let objectID: String
+    let title: String
+    let points: Int
+    let url: String
+}
