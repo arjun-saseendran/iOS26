93c003fd5b045589f7efd8b46db6738eb3bf1072
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-06-05 07:50:39 +0530

    config app folder structure

---
+++ TimerApp/TimerApp/TimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerAppApp.swift
+//  TimerApp
+//
+//  Created by Arjun Saseendran on 05/06/26.
+//
+
+import SwiftUI
+
+@main
+struct TimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
