3b794c6e41092559988fba833f9e161751aa8f4b
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-06-06 09:13:55 +0530

    add circular picker view model

--- TimerApp/TimerApp/ViewModels/CircularPickerViewModel.swift
+++ TimerApp/TimerApp/ViewModels/CircularPickerViewModel.swift
@@ -5,3 +5,14 @@
 //  Created by Arjun Saseendran on 05/06/26.
 //
 
+import SwiftUI
+import Combine
+
+final class CircularPickerViewModel: ObservableObject {
+    @Published var selectedValue: Int = 5
+    let totalValue: Int = 12
+
+    func selectValue(_ value: Int) {
+        selectedValue = value
+    }
+}
