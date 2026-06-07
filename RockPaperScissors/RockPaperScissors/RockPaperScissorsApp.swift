d4f68c574250822ddada038061f3aaba3ad8c104
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-06-07 06:34:50 +0530

    config app and add move

---
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Arjun Saseendran on 07/06/26.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
