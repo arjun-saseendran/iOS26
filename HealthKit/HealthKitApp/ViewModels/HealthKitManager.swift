b40a38d3d4505adf42e6b7d9cdd6f6e612663d71
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-06-08 10:25:58 +0530

    add health kit manager

--- HealthKit/HealthKitApp/ViewModels/HealthKitManager.swift
+++ HealthKit/HealthKitApp/ViewModels/HealthKitManager.swift
@@ -5,3 +5,46 @@
 //  Created by Arjun Saseendran on 08/06/26.
 //
 
+import SwiftUI
+import HealthKit
+import Combine
+
+final class HealthKitManager: ObservableObject {
+    static let shared = HealthKitManager()
+    let healthStore = HKHealthStore()
+
+    @Published var isAuthorized: Bool = false
+    @Published var healthData: [HealthMetric : [HealthRecord]] = [:]
+    @Published var isLoading: Bool = false
+
+    init(){
+        Task {
+            await checkAndRequestAuthorization()
+        }
+    }
+
+    func checkAndRequestAuthorization() async{
+        guard HKHealthStore.isHealthDataAvailable() else {
+            return
+        }
+        let typesToRead = Set(HealthMetric.allCases.map { $0.healthKitType})
+
+        do {
+            try await healthStore.requestAuthorization(toShare: [], read: typesToRead)
+            await MainActor.run {
+                isAuthorized = true
+                Task {
+                    await fetchAllData()
+                }
+            }
+        }catch {
+            print("Auth failed: \(error.localizedDescription)")
+        }
+    }
+
+    func fetchAllData() async{
+
+    }
+
+
+}
