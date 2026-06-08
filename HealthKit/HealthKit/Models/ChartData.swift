ff3807634f0912645fc557343c65a0037d0d4c69
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-06-08 07:33:55 +0530

    add chart data

--- HealthKit/HealthKit/Models/ChartData.swift
+++ HealthKit/HealthKit/Models/ChartData.swift
@@ -5,3 +5,11 @@
 //  Created by Arjun Saseendran on 08/06/26.
 //
 
+import Foundation
+
+struct ChartData {
+    let date: Date
+    let values: Double
+    let barX: CGFloat
+    let barHeight: CGFloat
+}
