9e64aa78ee825517ffbfef7d5899290304ec9ba4
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-06-08 09:04:51 +0530

    update health kit metric

--- HealthKit/HealthKitApp/Models/HealthMetric.swift
+++ HealthKit/HealthKitApp/Models/HealthMetric.swift
@@ -28,4 +28,50 @@ enum HealthMetric: String, CaseIterable {
 
         }
     }
+    var color: Color {
+        switch self {
+        case .calories: return .orange
+        case .steps: return .blue
+        case .sandHours: return .green
+        case .excerciseMinutes: return .red
+        case .hertRate: return .pink
+        case .flightsClimbed: return .indigo
+        }
+    }
+    var unit: String {
+        switch self {
+        case .calories: return "cal"
+        case .steps: return "steps"
+        case .sandHours: return "hrs"
+        case .excerciseMinutes: return "min"
+        case .hertRate: return "bpm"
+        case .flightsClimbed: return "flights"
+        }
+    }
+    var healthKitType: HKQuantityType {
+        switch self {
+        case .calories: return HKQuantityType(.activeEnergyBurned)
+        case .steps: return HKQuantityType(.stepCount)
+        case .sandHours: return HKQuantityType(.appleStandTime)
+        case .excerciseMinutes: return HKQuantityType(.appleExerciseTime)
+        case .hertRate: return HKQuantityType(.heartRate)
+        case .flightsClimbed: return HKQuantityType(.flightsClimbed)
+        }
+    }
+    var healthKitUint: HKUnit {
+        switch self {
+        case .calories: return .kilocalorie()
+        case .steps: return .count()
+        case .sandHours: return .hour()
+        case .excerciseMinutes: return .minute()
+        case .hertRate: return HKUnit.count().unitDivided(by: .minute())
+        case .flightsClimbed: return .count()
+        }
+    }
+    var statisticsOptions: HKStatisticsOptions {
+        switch self {
+        case .hertRate: return .discreteAverage
+        default: return .cumulativeSum
+        }
+    }
 }
