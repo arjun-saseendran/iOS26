095ad1cf6f0c7abddb53214810fadd1351f9072f
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-06-17 12:38:51 +0530

    add metric detail view

--- HealthKitApp/HealthKitApp/Views/MetricDetailView.swift
+++ HealthKitApp/HealthKitApp/Views/MetricDetailView.swift
@@ -5,15 +5,62 @@
 //  Created by Arjun Saseendran on 08/06/26.
 //
 
+import Charts
 import SwiftUI
 
 struct MetricDetailView: View {
+    @StateObject var healthKit = HealthKitManager.shared
     let metric: HealthMetric
+    @State private var selectedData: ChartData?
+
+    var records: [HealthRecord] {
+        healthKit.healthData[metric] ?? []
+
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Chart(records) { record in
+
+                BarMark(
+                    x: .value("Date", record.date, unit: .day),
+                    y: .value("Value", record.value)
+                )
+                .foregroundStyle(metric.color.gradient)
+            }
+            .frame(height: 200)
+            .padding()
+            .chartOverlay { proxy in
+                GeometryReader { geo in
+
+                }
+            }
+
+        }
+
+    }
+    private func findClosestRecord(
+        at xPosition: CGFloat,
+        proxy: ChartProxy,
+        size: CGSize
+    ) -> HealthRecord? {
+        let plotwidth = size.width
+        let barWidth = plotwidth / CGFloat(records.count)
+
+        var closestRecord: HealthRecord?
+        var minDistance: CGFloat = .infinity
+
+        for record in records {
+            guard let xPos = proxy.position(forX: record.date) else { continue }
+
+            let barCenter = xPos + (barWidth / 2)
+            let distance = abs(barCenter - xPosition)
+            if distance < minDistance {
+                minDistance = distance
+                closestRecord = record
+            }
+
+        }
+        return closestRecord
     }
 }
-
-//#Preview {
-//    MetricDetailView()
-//}
