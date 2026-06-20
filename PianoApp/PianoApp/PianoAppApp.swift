9225e119b6ea5f42e91178c50045aba90eeb0a60
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-06-20 12:40:10 +0530

    config app

---
+++ PianoApp/PianoApp/PianoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PianoAppApp.swift
+//  PianoApp
+//
+//  Created by Arjun Saseendran on 20/06/26.
+//
+
+import SwiftUI
+
+@main
+struct PianoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
