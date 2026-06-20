1772ca47a7537d0812af47461907605bfec4bb7f
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-06-20 12:12:48 +0530

    add views

--- 3DObjectApp/3DObjectApp/ContentView.swift
+++ 3DObjectApp/3DObjectApp/ContentView.swift
@@ -5,6 +5,8 @@
 //  Created by Arjun Saseendran on 18/06/26.
 //
 
+import ARKit
+import QuickLook
 import SwiftUI
 
 enum Item: String, Identifiable, CaseIterable {
@@ -35,18 +37,110 @@ enum Item: String, Identifiable, CaseIterable {
 }
 
 struct ContentView: View {
+    @State private var selectedItem: Item?
+    @State private var showItem = false
 
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(Item.allCases) {
+                item in
+                HStack {
+                    Text(item.icon)
+                    Text(item.name)
+                    Spacer()
+                }
+                .font(.title2)
+                .padding()
+                .contentShape(.rect)
+                .onTapGesture {
+                    selectedItem = item
+                }
+
+            }
+            .navigationTitle("3D Item Explorer")
+            .onChange(of: selectedItem) { _, _ in
+                showItem.toggle()
+
+            }
+            .sheet(isPresented: $showItem) {
+                if let selectedItem {
+                    NavigationStack {
+                        ARQuicklookView(name: selectedItem.rawValue)
+                            .toolbar {
+                                ToolbarItem(placement: .topBarTrailing) {
+                                    Button {
+                                        showItem.toggle()
+
+                                    } label: {
+                                        Image(systemName: "xmark.circle")
+                                            .font(.title3)
+                                            .foregroundStyle(.primary)
+
+                                    }
+                                    .buttonStyle(.plain)
+
+                                }
+
+                            }
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct ARQuicklookView: UIViewControllerRepresentable {
+    var name: String
+    func makeCoordinator() -> ARQuicklookView.Coordinator {
+        Coordinator(self)
+    }
+
+    func makeUIViewController(context: Context) -> QLPreviewController {
+        let controller = QLPreviewController()
+        controller.dataSource = context.coordinator
+        return controller
+    }
+
+    func updateUIViewController(
+        _ uiViewController: QLPreviewController,
+        context: Context
+    ) {
+
+    }
+
+    class Coordinator: NSObject, QLPreviewControllerDataSource {
+        var parent: ARQuicklookView
+        private lazy var fileURL: URL = Bundle.main.url(
+            forResource: parent.name,
+            withExtension: "usdz"
+        )!
+        init(_ parent: ARQuicklookView) {
+            self.parent = parent
+            super.init()
+        }
+
+        func numberOfPreviewItems(in controller: QLPreviewController) -> Int {
+            return 1
+        }
+        func previewController(
+            _ controller: QLPreviewController,
+            previewItemAt index: Int
+        ) -> QLPreviewItem {
+            guard
+                let fileURL = Bundle.main.url(
+                    forResource: parent.name,
+                    withExtension: "usdz"
+                )
+            else {
+                fatalError("Unable to find \(parent.name).usdz")
+            }
+            let item = ARQuickLookPreviewItem(fileAt: fileURL)
+            return item
+        }
+
+    }
 }
