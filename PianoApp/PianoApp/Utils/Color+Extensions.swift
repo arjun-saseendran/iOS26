3090ba0dd4348120a70155043063301bca939de3
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-06-20 14:44:17 +0530

    add color extesion

---
+++ PianoApp/PianoApp/Utils/Color+Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Color+Extensions.swift
+//  PianoApp
+//
+//  Created by Arjun Saseendran on 20/06/26.
+//
+
+import SwiftUI
+
+extension Color {
+    static let pianoWhiteKey = Color.white
+    static let pianoBlackKey = Color.black
+    static let pianoHighlightColor = Color.blue
+    
+
+}
