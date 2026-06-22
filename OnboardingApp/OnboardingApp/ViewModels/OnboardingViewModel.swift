ee6755ef2b90fe20a6bb69a35cb2c0f46cbcb326
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-06-22 12:07:32 +0530

    add functions to onboarding view model

--- OnboardingApp/OnboardingApp/ViewModels/OnboardingViewModel.swift
+++ OnboardingApp/OnboardingApp/ViewModels/OnboardingViewModel.swift
@@ -21,6 +21,35 @@ final class OnboardingViewModel {
     init(steps: [OnboardingStep], onComplete: @escaping () -> Void = {}) {
         self.steps = steps
         self.onboardingCompletion = onComplete
+    }
 
+    var isLastStep: Bool {
+        guard let currentIndex = currentIndex else { return false}
+        return currentIndex >= steps.count - 1
+    }
+
+    var currentSteps: OnboardingStep {
+        guard let currentIndex = currentIndex else {
+            return steps.first!
+        }
+        return steps[safe: currentIndex] ?? steps.first!
+    }
+
+    func updateScreenSize(_ size: CGSize) {
+        self.screenSize = size
+    }
+
+    func updateCurrentIndex(_ index: Int) {
+        guard index >= 0 && index < steps.count else { return }
+        if index != currentIndex {
+            currentIndex = index
+        }
+    }
+
+}
+
+extension Array {
+    subscript (safe index: Int) -> Element? {
+        return indices.contains(index) ? self[index] : nil
     }
 }
