2266c56e193ee3aea38498f7455457b36758c0a3
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-06-22 11:18:50 +0530

    config app

---
+++ OnboardingApp/OnboardingApp/OnboardingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingAppApp.swift
+//  OnboardingApp
+//
+//  Created by Arjun Saseendran on 22/06/26.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
