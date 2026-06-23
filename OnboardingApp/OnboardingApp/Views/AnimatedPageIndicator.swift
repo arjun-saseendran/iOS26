f93e239e36ebdfd6acf8d0bd74b1fdb397d04364
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-06-23 16:06:08 +0530

    add animated page indicator view

---
+++ OnboardingApp/OnboardingApp/Views/AnimatedPageIndicator.swift
@@ -0,0 +1,32 @@
+//
+//  AnimatedPageIndicator.swift
+//  OnboardingApp
+//
+//  Created by Arjun Saseendran on 23/06/26.
+//
+
+import SwiftUI
+
+struct AnimatedPageIndicator: View {
+    let totalPage: Int
+    let currentIndex: Int
+    let indicatorSpacing: CGFloat = 12
+    let indicatorHeight: CGFloat = 7
+    let activeIndicatorWidth: CGFloat = 20
+    let inactiveIndicatorWidth: CGFloat = 7
+
+    var body: some View {
+        HStack(spacing: indicatorSpacing) {
+            ForEach(0..<totalPage, id: \.self) { index in
+                Capsule()
+                    .foregroundStyle(.white.gradient)
+                    .frame(
+                        width: currentIndex == index
+                            ? activeIndicatorWidth : inactiveIndicatorWidth,
+                        height: indicatorHeight
+                    )
+                    .animation(.easeInOut(duration: 0.3), value: currentIndex)
+            }
+        }
+    }
+}
