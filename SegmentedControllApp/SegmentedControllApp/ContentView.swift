cd606166341e953029c9ea242b48034b9605d8f9
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-06-24 15:16:39 +0530

    add data model

--- SegmentedControllApp/SegmentedControllApp/ContentView.swift
+++ SegmentedControllApp/SegmentedControllApp/ContentView.swift
@@ -7,6 +7,36 @@
 
 import SwiftUI
 
+enum AppTab: String, CaseIterable {
+    case swiftUI = "SwiftUI"
+    case iOS = "iOS"
+    case UIKit = "UIKit"
+
+    var color: Color {
+        switch self {
+        case .swiftUI:
+            return .orange
+        case .iOS:
+            return .blue
+        case .UIKit:
+            return .purple
+        }
+    }
+
+    var icon: String {
+        switch self {
+        case .swiftUI:
+            return "swift"
+        case .iOS:
+            return "apple.logo"
+        case .UIKit:
+            return "macwindow"
+        }
+    }
+
+
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
