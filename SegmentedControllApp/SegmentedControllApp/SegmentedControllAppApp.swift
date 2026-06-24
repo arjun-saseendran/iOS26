16ad307d65eba2900b8c8030029f73c2c3cb7f03
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-06-24 15:09:50 +0530

    config app

---
+++ SegmentedControllApp/SegmentedControllApp/SegmentedControllAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SegmentedControllAppApp.swift
+//  SegmentedControllApp
+//
+//  Created by Arjun Saseendran on 24/06/26.
+//
+
+import SwiftUI
+
+@main
+struct SegmentedControllAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
