2002a1f076f9a4d99341664755b1a5d5c48c9820
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-07-01 11:22:29 +0530

    add extensions

---
+++ ChatBotApp/ChatBotApp/Utils/AttributedString+Extensions.swift
@@ -0,0 +1,54 @@
+import Foundation
+//
+//  AttributedString+Extensions.swift
+//  ChatBotApp
+//
+//  Created by Arjun Saseendran on 01/07/26.
+//
+import SwiftUI
+
+extension AttributedString {
+    init(systemMarkdown markdownString: String) throws {
+        var output = try AttributedString(
+            markdown: markdownString,
+            options: .init(
+                allowsExtendedAttributes: true,
+                interpretedSyntax: .full,
+                failurePolicy: .returnPartiallyParsedIfPossible
+
+            ),
+            baseURL: nil
+        )
+        for (intentBlock, intentRange) in output.runs[
+            AttributeScopes.FoundationAttributes.PresentationIntentAttribute
+                .self
+        ].reversed() {
+            guard let intentBlock = intentBlock else { continue }
+            for intent in intentBlock.components {
+                switch intent.kind {
+                case .header(let level):
+                    switch level {
+                    case 1:
+                        output[intentRange].font = .system(.title).bold()
+                    case 2:
+                        output[intentRange].font = .system(.title2).bold()
+                    case 3:
+                        output[intentRange].font = .system(.title3).bold()
+                    default:
+                        break
+                    }
+                default:
+                    break
+                }
+            }
+            if intentRange.lowerBound != output.startIndex {
+                output.characters.insert(
+                    contentsOf: "\n\n",
+                    at: intentRange.lowerBound
+                )
+            }
+
+        }
+        self = output
+    }
+}
