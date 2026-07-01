546779f4350c25166cefd84b9aa79266b3b4127e
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-07-01 09:59:56 +0530

    config app

---
+++ ChatBotApp/ChatBotApp/ChatBotAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatBotAppApp.swift
+//  ChatBotApp
+//
+//  Created by Arjun Saseendran on 01/07/26.
+//
+
+import SwiftUI
+
+@main
+struct ChatBotAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
